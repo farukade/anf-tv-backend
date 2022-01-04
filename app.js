bc00ef3db64830fce8136590180712400af20ef4
unknown <farukadekunle@gmail.com>
2022-01-04 23:39:30 +0100

    user-news model-controller implementation

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var newsRouter = require('./routes/news');
 
 var app = express();
 
@@ -21,6 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+
+app.use(cors());
+
+const db = require("./models/index");
+db.sequelize.sync();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Create new user
+router.post('/create-user', userController.userControllers.createUser);
+
+// Get All Users
+router.get('/get-users', userController.userControllers.getAllUsers);
+
+// Get Single User
+router.get('/get-user/:id', userController.userControllers.getOneUser);
+
+// Get Single User
+router.delete('/delete-user/:id', userController.userControllers.deleteUser);
+
+// Update user details
+router.put('/update-user/:id', userController.userControllers.updateUser);
 
 module.exports = router;
