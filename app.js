2f2fe8912b2cf8c980d35a0d0786941018ffa343
unknown <farukadekunle@gmail.com>
2022-02-01 00:32:33 +0100

    request size limit edited on app js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/news', newsRouter);
 
-app.use(cors({ origin: 'https://anf-tv.netlify.app', credentials: true}));
+app.use(cors());
 
 app.get('/user/:id', function (req, res, next) {
   res.json({user: 'CORS enabled'})
